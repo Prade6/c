30bc7528390e4b37961d088c1cdaaa352255c5c9
pradeep kumar <pradesep6@gmail.com>
2016-05-20 22:01:43 +0530

    basic c programs

---
+++ basicc.c
@@ -0,0 +1,111 @@
+/* sum of n no  
+
+#include <stdio.h>
+
+int main()
+{
+	int n,sum=0,i,value;
+	printf("enter n value\n");
+	scanf("%d",&n);
+    printf("enter %d value\n",n);
+	for (int i = 1; i<=n; i++)
+	{
+		scanf("%d",&value);
+		sum=sum+value;
+	}
+	printf("sum - %d \n",sum);
+	return 0;
+}
+
+*/ 
+// ____________________________________________________________________________________________ 
+
+/* sum of no 
+#include <stdio.h>
+
+int main()
+{
+	int n,sum=0;
+	printf("enter n value\n");
+	scanf("%d",&n);
+	for(int i=1; i<=n; i++)
+	{
+		sum=sum+i;
+	}
+	printf("sum of no's upto n is %d \n",sum);
+} 
+
+*/ 
+// ____________________________________________________________________________________________ 
+
+/* sum of no 
+
+#include<stdio.h>
+
+int main()
+{
+	int n,sum=0;
+	printf("enter n value\n");
+	scanf("%d",&n);
+	for (int i = 1; i <= n; i++)
+	{
+			if(i%2==0)
+		{
+			sum=sum+i;
+		}
+
+	}
+	printf("sum of even no's - %d \n",sum);
+
+}
+*/ 
+// ____________________________________________________________________________________________ 
+
+
+/* fibo 
+
+#include<stdio.h>
+
+int main()
+{
+	int a=0,b=1,c,i,n;
+	printf("enter len of fibo\n");
+	scanf("%d",&n);
+	printf("the series is - \n");
+	printf("%d\n%d\n",a,b);
+	for (int i = 0; i < n; i++)
+	{
+		c=a+b;
+		a=b;
+		b=c;
+ 		printf("%d\n", c);
+	}
+
+}
+____________________________________________________________________________________________ */ 
+
+#include <stdio.h>
+
+int main()
+{
+	for (i=1; i++)
+	{
+	
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
